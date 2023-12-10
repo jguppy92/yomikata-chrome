e8b7f8d1767f4955f05b950cb84ffb0d3b36a624
James Guppy <j.guppy321@gmail.com>
2023-12-10 16:25:07 +0000

    Hooked up kanji dictionary API to the React app. Pulling data currentyl using hardcoded variable, next steps to set the selected kanji variable when a valid character is clicked in the browser window.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { getKanji } from './lib/api';
 
 function App() {
+  const [kanji, setKanji] = React.useState('')
+
+  const testKanji = '酒'
+
+  React.useEffect(() => {
+    const getData = async () => {
+      try {
+        const response = await getKanji(testKanji)
+        setKanji(response.data)
+      } catch(err) {
+        console.log(err)
+      }
+    }
+    getData()
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {kanji.kanji}
         </p>
         <a
           className="App-link"
