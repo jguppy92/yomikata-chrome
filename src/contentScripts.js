d85f78ffec005669f9d86ce41e07869bf3027adf
James Guppy <j.guppy321@gmail.com>
2024-01-18 20:07:36 +0000

    Moved wrap kanji function back to content script as it's altering the page's HTML directly.

--- src/contentScripts.js
+++ src/contentScripts.js
@@ -26,8 +26,39 @@ const uiElements = `
       <p>
         Meanings: <span id="meanings"></span>
       </p>
-    </div>
-    `
+  </div>
+`
+function wrapKanjiCharacters(node) {
+  if (node.nodeType === Node.TEXT_NODE) {
+      const contentText = node.textContent
+      let wrappedContent = ''
+      for (let i = 0; i < contentText.length; i++) {
+          const char = contentText[i]
+
+          // Check if the character is a kanji character
+          if (regexKanji.test(char)) {
+              wrappedContent += `<span class="kanji">${char}</span>`
+          } else {
+              wrappedContent += char
+          }
+      }
+      // Replace the original text node with the wrapped content
+      const spanContainer = document.createElement('span')
+      spanContainer.innerHTML = wrappedContent
+      node.parentNode.replaceChild(spanContainer, node)
+  } else if (node.nodeType === Node.ELEMENT_NODE) {
+      // Recursively process child nodes
+      for (let i = 0; i < node.childNodes.length; i++) {
+          wrapKanjiCharacters(node.childNodes[i])
+      }
+  }
+}
+
+// Call the function when the page is loaded
+window.onload = function () {
+  const contentElement = document.getElementsByTagName('body')[0]
+  wrapKanjiCharacters(contentElement);
+}
 
 // Create the popup div that will display the data.
 const popupDiv = document.createElement('div')
